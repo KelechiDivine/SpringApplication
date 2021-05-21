e66920e60c5568f3d7689af6922ba673dbdff55b
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-21 11:18:21 +0100

    clean up errors

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,24 +1,16 @@
 package com.example.demo.student;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Component;
 
-=======
->>>>>>> 1b8e8b071c3968f8b2bd19dbb2283ba6de9cf241
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-<<<<<<< HEAD
+
 @Component
 public class StudentService {
 	
-=======
-public class StudentService {
-	
-	
->>>>>>> 1b8e8b071c3968f8b2bd19dbb2283ba6de9cf241
-	public List<Student> getStudents(){
+	public List<Student> getStudents() {
 		return List.of(
 				new Student(
 						1l,
@@ -30,3 +22,4 @@ public class StudentService {
 		);
 	}
 }
+
