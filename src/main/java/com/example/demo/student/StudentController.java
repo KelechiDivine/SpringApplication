1b8e8b071c3968f8b2bd19dbb2283ba6de9cf241
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-21 11:00:34 +0100

    created an end point for student data

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+//Requested for a request mapping that allows and gives a new address to our api
+@RequestMapping(path = "api/v1/student")
+
+public class StudentController {
+	private final StudentService studentService;
+	
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	
+	@GetMapping
+	
+	public List<Student> getStudents() {
+	
+	}
+	
+}
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+	
+	
+	public List<Student> getStudents(){
+		return List.of(
+				new Student(
+						1l,
+						"Mariam",
+						21,
+						LocalDate.of(2000, Month.JANUARY, 5),
+						"mariam.jamal@gmail.com"
+				)
+		);
+	}
+}
